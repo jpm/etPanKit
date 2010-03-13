f7bd95b1807a359457c4aa78abebf342fd078e48
DINH Viet Hoa <hoa@sprw.me>
2010-03-13 00:22:27 +0000

    fixed extracted subject for french support

--- Source/LEPMessageHeader.m
+++ Source/LEPMessageHeader.m
@@ -335,6 +335,10 @@ static inline int skip_subj_refwd(char * subj, size_t * begin,
 				cur_token += 2;
 				prefix = 1;
 			}
+			else if (strncasecmp(subj + cur_token, "tr", 2) == 0) {
+				cur_token += 2;
+				prefix = 1;
+			}
 		}
 	}
 	
