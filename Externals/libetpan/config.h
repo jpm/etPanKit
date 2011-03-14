13027e6c1b6ddbd79298ed5c494b18726999c42d
DINH Viet Hoa <hoa@sprw.me>
2011-03-14 22:54:44 +0000

    updated to libetpan-1.0cvs35

--- Externals/libetpan/config.h
+++ Externals/libetpan/config.h
@@ -119,7 +119,7 @@
 #define LIBETPAN_REENTRANT 1
 
 /* Define this to the version of libEtPan */
-#define LIBETPAN_VERSION "1.0-dev-20110312"
+#define LIBETPAN_VERSION "1.0-dev-20110314"
 
 /* Define this to the major version of libEtPan */
 #define LIBETPAN_VERSION_MAJOR 1
